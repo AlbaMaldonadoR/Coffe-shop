7ae0edca18d94736de00b1526c880e72edcb02fa
AlbaMaldonadoR <131353973+AlbaMaldonadoR@users.noreply.github.com>
2023-06-02 12:33:51 +0200

    Add files via upload

---
+++ cafe/components/reserva.js
@@ -0,0 +1,114 @@
+let monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December'];
+
+let currentDate = new Date();
+let currentDay = currentDate.getDate();
+let monthNumber = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+
+let dates = document.getElementById('dates');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+
+let prevMonthDOM = document.getElementById('prev-month');
+let nextMonthDOM = document.getElementById('next-month');
+
+month.textContent = monthNames[monthNumber];
+year.textContent = currentYear.toString();
+
+prevMonthDOM.addEventListener('click', ()=>lastMonth());
+nextMonthDOM.addEventListener('click', ()=>nextMonth());
+
+
+
+const writeMonth = (month) => {
+
+    for(let i = startDay(); i>0;i--){
+        dates.innerHTML += ` <div class="calendar__date calendar__item calendar__last-days">
+            ${getTotalDays(monthNumber-1)-(i-1)}
+        </div>`;
+    }
+
+    for(let i=1; i<=getTotalDays(month); i++){
+        if(i===currentDay) {
+            dates.innerHTML += ` <div class="calendar__date calendar__item calendar__today">${i}</div>`;
+        }else{
+            dates.innerHTML += ` <div class="calendar__date calendar__item">${i}</div>`;
+        }
+    }
+}
+
+const getTotalDays = month => {
+    if(month === -1) month = 11;
+
+    if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
+        return  31;
+
+    } else if (month == 3 || month == 5 || month == 8 || month == 10) {
+        return 30;
+
+    } else {
+
+        return isLeap() ? 29:28;
+    }
+}
+
+const isLeap = () => {
+    return ((currentYear % 100 !==0) && (currentYear % 4 === 0) || (currentYear % 400 === 0));
+}
+
+const startDay = () => {
+    let start = new Date(currentYear, monthNumber, 1);
+    return ((start.getDay()-1) === -1) ? 6 : start.getDay()-1;
+}
+
+const lastMonth = () => {
+    if(monthNumber !== 0){
+        monthNumber--;
+    }else{
+        monthNumber = 11;
+        currentYear--;
+    }
+
+    setNewDate();
+}
+
+const nextMonth = () => {
+    if(monthNumber !== 11){
+        monthNumber++;
+    }else{
+        monthNumber = 0;
+        currentYear++;
+    }
+
+    setNewDate();
+}
+
+const setNewDate = () => {
+    currentDate.setFullYear(currentYear,monthNumber,currentDay);
+    month.textContent = monthNames[monthNumber];
+    year.textContent = currentYear.toString();
+    dates.textContent = '';
+    writeMonth(monthNumber);
+}
+
+writeMonth(monthNumber);
+document .querySelector(".openChatBtn") .addEventListener("click", openForm);
+document.querySelector(".close").addEventListener("click", closeForm);
+function openForm() {
+   document.querySelector(".openChat").style.display = "block";
+}
+function closeForm() {
+   document.querySelector(".openChat").style.display = "none";
+}
+function movilSocial() {
+    window.open('http://twitter.com', 'Twitter', 'width=915, height=590');
+  }
+  function correoSocial() {
+    window.open('https://www.google.com/intl/es/gmail/about/', 'Twitter', 'width=915, height=590');
+  }
+  function whatsappSocial() {
+    window.open('https://www.whatsapp.com/?lang=es', 'Twitter', 'width=915, height=590');
+  }
+  function instagramSocial() {
+    window.open('https://www.instagram.com/', 'Twitter', 'width=915, height=590');
+  }
